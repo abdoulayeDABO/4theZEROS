72877a2a78817e952a8a74506a9e086e51a13cc1
codlabsn <abdoulayeleom10@gmail.com>
2024-01-19 21:21:59 +0000

    auth/03

---
+++ PHP/auth/03/.history/home_20240119211907.php
@@ -0,0 +1,33 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // destroy the session
+            session_destroy();
+            header("Location: index.php");
+        }
+    ?>
+
+    <?php
+        // protect the home page
+        if(!isset($_SESSION['is_authenticated']) && $_SESSION['is_authenticated'] != true){
+            header("Location: index.php");
+        }
+        echo "<div>Welcome". $_COOKIE["user_name"]."</div>";
+        echo "<div>You're Logged in</div>";
+    ?>
+
+    <form method="post">
+        <input type="submit" value="Logout">
+    </form>
+
+</body>
+</html>
